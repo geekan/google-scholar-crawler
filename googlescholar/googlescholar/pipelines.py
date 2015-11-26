9b2d66934f6dd279d63627a5efb96412b83f02c5
geekan <geekan@foxmail.com>
2015-11-26 21:41:42 +0800

    Update pipelines.py

--- googlescholar/googlescholar/pipelines.py
+++ googlescholar/googlescholar/pipelines.py
@@ -3,8 +3,6 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-import redis
-
 
 from scrapy import signals
 
@@ -26,25 +24,3 @@ class JsonWithEncodingPipeline(object):
 
     def spider_closed(self, spider):
         self.file.close()
-
-
-class RedisPipeline(object):
-
-    def __init__(self):
-        self.r = redis.StrictRedis(host='localhost', port=6379)
-
-    def process_item(self, item, spider):
-        if not item['id']:
-            print 'no id item!!'
-
-        str_recorded_item = self.r.get(item['id'])
-        final_item = None
-        if str_recorded_item is None:
-            final_item = item
-        else:
-            ritem = eval(self.r.get(item['id']))
-            final_item = dict(item.items() + ritem.items())
-        self.r.set(item['id'], final_item)
-
-    def spider_closed(self, spider):
-        return
